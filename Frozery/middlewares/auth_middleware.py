918e350dcd9d3b55cc4d4932329c782a52e2776c
Madni Sadiq <2017ee189@student.uet.edu.pk>
2021-02-23 03:05:27 +0500

    Add files via upload

--- Frozery/middlewares/auth_middleware.py
+++ Frozery/middlewares/auth_middleware.py
@@ -5,8 +5,9 @@ def auth_middleware(get_response):
 
     def middleware(request):
         returnUrl = request.META['PATH_INFO']
-        Customer = request.session.get('Customer') 
-        if not Customer:
+        Customer = request.session.get('Customer')
+        admin = request.session.get('admin')
+        if (not Customer) and (not admin):
             return redirect(f'/accounts/login?return_url={returnUrl}')
         
         response = get_response(request)
